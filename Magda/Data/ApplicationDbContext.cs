ae4f459238f7ef1d4943210289141c317175dd0d
pwgawzynski <pgawzynski.backup@gmail.com>
2022-11-26 11:03:48 +0100

    model done
    Order model is done, views in progres

--- Magda/Data/ApplicationDbContext.cs
+++ Magda/Data/ApplicationDbContext.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Magda.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Magda.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Order> Order { get; set; }
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+            : base(options) 
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
---
+++ Magda/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Magda.Models
+{
+	public class Order
+	{
+
+		
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string? Id { get; set; }
+		public string Name { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+		public enum EventType 
+		{
+			PublicEvent,
+			PrivateEvent,
+		}
+		public EventType OrderType { get; set; }
+        public string RoomName { get; set; }
+        public string AdditionalRemarks { get; set; }
+
+        public Order()
+		{
+		}
+	}
+}
+
